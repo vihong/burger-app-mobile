4359dd83da7e75b151c61f91e47cf9e2a2b3392b
Vi <probamaths@gmail.com>
2024-05-30 22:51:59 +0200

    ✨ F04 : MESSAGES > add pull to refresh

--- src/components/screens/MessagesScreen.tsx
+++ src/components/screens/MessagesScreen.tsx
@@ -1,18 +1,36 @@
-import React from "react"
-import { FlatList, View } from "react-native"
-import Text from "../designSystem/atoms/Text"
+import React, { useState } from "react"
+import { FlatList } from "react-native"
 import ListItem from "../designSystem/molecules/ListItem"
 import Screen from "../designSystem/atoms/Screen"
 import ListItemSeparator from "../designSystem/atoms/ListItemSeparator"
 import ListItemDeleteAction from "../designSystem/atoms/ListItemDeleteAction"
+import { Message } from "../../types/Message"
+import { deepClone } from "../../utils/typescript"
 
-const messages = [
+const fakeMessages: Message[] = [
   { id: 1, title: "T1", subtitle: "D1", image: require("../../assets/mosh.jpg") },
   { id: 2, title: "T2", subtitle: "D2", image: require("../../assets/mosh.jpg") },
   { id: 3, title: "T3", subtitle: "D3", image: require("../../assets/mosh.jpg") },
 ]
 
 export default function MessagesScreen() {
+  const [messages, setMessages] = useState<Message[]>(fakeMessages)
+  const [isRefreshing, setIsRefreshing] = useState(false)
+
+  const handleDeleteMessage = (idMessageToDelete: number) => {
+    const messagesCopy = deepClone(messages)
+    const updatedMessages = messagesCopy.filter((message) => message.id !== idMessageToDelete)
+    setMessages(updatedMessages)
+  }
+
+  const handleRefreshMessages = () => {
+    setIsRefreshing(true)
+    setTimeout(() => {
+      setMessages([{ id: 2, title: "T2", subtitle: "D2", image: require("../../assets/mosh.jpg") }])
+      setIsRefreshing(false)
+    }, 2000)
+  }
+
   return (
     <Screen>
       <FlatList
@@ -25,11 +43,13 @@ export default function MessagesScreen() {
             imageSource={message.image}
             onPress={() => console.log("message.title: ", message.title)}
             renderRightActions={() => (
-              <ListItemDeleteAction onPress={() => console.log("Deleted")} />
+              <ListItemDeleteAction onPress={() => handleDeleteMessage(message.id)} />
             )}
           />
         )}
         ItemSeparatorComponent={() => <ListItemSeparator />}
+        refreshing={isRefreshing}
+        onRefresh={handleRefreshMessages}
       />
     </Screen>
   )
---
+++ src/types/Message.ts
@@ -0,0 +1,8 @@
+import { ImageSourcePropType } from "react-native"
+
+export type Message = {
+  id: number
+  title: string
+  subtitle: string
+  image: ImageSourcePropType
+}
