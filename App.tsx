f0b9c5a891e2dbab445ac2ed35b716ac86c42eba
Vi <probamaths@gmail.com>
2024-06-02 13:35:29 +0200

    ✨ M07 : APP > Create StackNavigator

--- App.tsx
+++ App.tsx
@@ -1,37 +1,19 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler"
 import DraftScreen from "./src/components/screens/DraftScreen"
-import FeedScreen from "./src/components/screens/FeedScreen"
 import MessagesScreen from "./src/components/screens/MessagesScreen"
 import ProductScreen from "./src/components/screens/ProductScreen"
 import ViewImageScreen from "./src/components/screens/ViewImageScreen"
-import WelcomeScreen from "./src/components/screens/WelcomeScreen"
 import AccountScreen from "./src/components/screens/AccountScreen"
 import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-
-const Stack = createNativeStackNavigator()
+import TabNavigator from "./src/components/navigators/TabNavigator"
+import StackNavigator from "./src/components/navigators/StackNavigator"
 
 export default function App() {
   return (
-    <GestureHandlerRootView>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{ headerStyle: { backgroundColor: "dodgerblue" }, headerShown: true }}
-          initialRouteName="Feed"
-        >
-          <Stack.Screen
-            name="Welcome"
-            component={WelcomeScreen}
-            options={{ headerStyle: { backgroundColor: "tomato" } }}
-          />
-          <Stack.Screen
-            name="Feed"
-            component={FeedScreen}
-            options={({ route }) => {
-              return { title: `Bonjour ${route?.params?.name}` }
-            }}
-          />
-        </Stack.Navigator>
+        <StackNavigator />
+        {/* <TabNavigator /> */}
       </NavigationContainer>
     </GestureHandlerRootView>
   )
---
+++ src/components/navigators/StackNavigator.tsx
@@ -0,0 +1,25 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import React from "react"
+import WelcomeScreen from "../screens/WelcomeScreen"
+import FeedScreen from "../screens/FeedScreen"
+
+const Stack = createNativeStackNavigator()
+
+export default function StackNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Feed">
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{ headerStyle: { backgroundColor: "tomato" } }}
+      />
+      <Stack.Screen
+        name="Feed"
+        component={FeedScreen}
+        options={({ route }) => {
+          return { title: `Bonjour ${route?.params?.name}` }
+        }}
+      />
+    </Stack.Navigator>
+  )
+}
