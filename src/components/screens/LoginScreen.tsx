5958da6c81d97dc57554ffba154ab85d3ff5e3f4
Vi <probamaths@gmail.com>
2024-06-09 14:09:09 +0200

    🚧 M04: LOGIN SCREEN > Add text inputs

--- src/components/screens/LoginScreen.tsx
+++ src/components/screens/LoginScreen.tsx
@@ -1,32 +1,66 @@
-import React from "react"
-import Text from "../designSystem/atoms/Text"
+import React, { useState } from "react"
 import Screen from "../designSystem/atoms/Screen"
 import Button from "../designSystem/atoms/Button"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { AuthRootStackParamList } from "../navigators/screenNavigators/AuthNavigator"
 import Logo from "../designSystem/atoms/Logo"
 import { StyleSheet, View } from "react-native"
+import TextInput from "../designSystem/atoms/TextInput"
 
 type LoginScreenProps = NativeStackScreenProps<AuthRootStackParamList, "LoginScreen">
 
 export default function LoginScreen({ navigation }: LoginScreenProps) {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handeSubmit = () => {
+    console.log(email, password)
+  }
+
   return (
-    <Screen>
+    <Screen style={styles.screen}>
       <View style={styles.logo}>
         <Logo />
       </View>
-      <Button title="Bonjour" onPress={() => navigation.navigate("WelcomeScreen")} />
+      <View style={styles.form}>
+        <TextInput
+          value={email}
+          onChange={(event) => setEmail(event.nativeEvent.text)}
+          placeholder="Email"
+          iconName="email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+          autoComplete="email"
+        />
+        <TextInput
+          value={password}
+          onChange={(event) => setPassword(event.nativeEvent.text)}
+          placeholder="Password"
+          iconName="lock"
+          autoCapitalize="none"
+          autoCorrect={false}
+          autoComplete="password"
+          secureTextEntry
+        />
+        <Button title="Login" onPress={handeSubmit} />
+      </View>
     </Screen>
   )
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "blue",
+  },
   logo: {
-    // borderWidth: 1,
-    // borderColor: "blue",
-    display: "flex",
     alignItems: "center",
     marginTop: 50,
     marginBottom: 30,
+  },
+  form: {
+    paddingHorizontal: 10,
   },
 })
